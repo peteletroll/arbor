17d12ba7c4708126ffc5d72c23c26c330e406ff9
peteletroll <>
2022-07-08 07:54:10 +0200

    FIX: NaN point velocities would prevent system from stopping calculations when energy was below threshold (from andraus)

--- src/physics/physics.js
+++ src/physics/physics.js
@@ -293,7 +293,7 @@
           if (that.integrator=='euler'){
             point.v = point.v.add(point.f.multiply(timestep)).multiply(1-that.friction);
           }else{
-            point.v = point.v.add(point.f.add(point._F.divide(point._m)).multiply(timestep*0.5)).multiply(1-that.friction);
+            point.v = point.v.add(point.f.add(point._F.divide(point.m)).multiply(timestep*0.5)).multiply(1-that.friction);
           }
           point.f.x = point.f.y = 0
 
