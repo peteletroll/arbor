30b5f1a735c42ad05ca869f39b79c2abe33e2982
peteletroll <pietro.cagnoni@gmail.com>
2022-07-12 17:08:11 +0200

    perl tool to remove $.each()

---
+++ src/physics/kill-each.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# $.each(active.springs, function(id, spring){
+#   spring.k = stiff
+# })             
+
+local $/ = undef;
+while (<>) {
+	s{
+		\$\.each\(
+		\s*
+		([\w\.]+) \s*, \s*
+		function \s* \( \s*
+		(\w+) \s* , \s* (\w+) \s*
+		\) \s* \{
+		\s* \n ([^\S\n]*)
+	}
+	{
+		my ($obj, $key, $val, $ind) = ($1, $2, $3, $4);
+		"for ($key in $obj) \{\n${ind}var $val = $obj\[$key\];\n$ind"
+	}gex;
+	print;
+}
+
